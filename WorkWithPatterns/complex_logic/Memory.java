b969348024adee69153c6470bec59680d9e52a66
MW69 <118361120+AbMa10@users.noreply.github.com>
2023-02-05 15:18:51 +0300

    Patterns

--- WorkWithPatterns/complex_logic/Memory.java
+++
@@ -1,9 +0,0 @@
-package complex_logic;
-
-import java.util.Arrays;
-
-public class Memory {
-public void load(long position, byte[] data) {
-        System.out.println("Memory load data " + Arrays.toString(data) + " to position: " + position);
-    }
-}
